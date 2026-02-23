a94e511b7a0480e774f1905b613391dd550b8dc7
Badr Klila <klilabader2@gmail.com>
2026-02-23 04:28:29 +0100

    fixed the paths of the microservices

--- backend/ApiGateway/src/main/java/tn/esprit/apigateway/ApiGatewayApplication.java
+++ backend/ApiGateway/src/main/java/tn/esprit/apigateway/ApiGatewayApplication.java
@@ -17,19 +17,32 @@ public class ApiGatewayApplication {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
 
-
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("appointment-service", r -> r
-                        .path("/EverCare/appointments/**")
+                        .path("/EverCare/appointments/**",
+                                "/EverCare/availabilities/**",
+                                "/EverCare/consultation-types/**",
+                                "/EverCare/medicaments/**",
+                                "/EverCare/prescriptions/**")
                         .uri("lb://APPOINTMENT-SERVICE"))
                 .route("activities-service", r -> r
-                        .path("/EverCare/activities/**", "/EverCare/admin/activities/**")
+                        .path("/EverCare/activities/**",
+                                "/EverCare/admin/activities/**")
                         .uri("lb://ACTIVITIES-SERVICE"))
                 .route("user-service", r -> r
-                        .path("/EverCare/admin/**", "/EverCare/auth/**,/EverCare/users/**,")
+                        .path("/EverCare/admin/**",
+                                "/EverCare/auth/**",
+                                "/EverCare/users/**")
                         .uri("lb://USER-SERVICE"))
+                .route("communication-service", r -> r
+                        .path("/api/calls/**",
+                                "/api/conversations/**")
+                        .uri("lb://COMMUNICATION-SERVICE"))
+                .route("medical-record-service", r -> r
+                        .path("/api/medical-records/**")
+                        .uri("lb://MEDICAL-RECORD-SERVICE"))
                 .build();
     }
 
